8c7645ed9f75f0e09b2a862113a419e6583b92a4
GSMagx <gsmagx@me.com>
2018-12-16 21:04:42 +0100

    Add KeyboardWasShow and Hide

--- VK/Controller/LoginFormController.swift
+++ VK/Controller/LoginFormController.swift
@@ -15,19 +15,44 @@ class LoginFormController: UIViewController {
     @IBOutlet weak var passwordInput: LoginTextField!
     @IBOutlet weak var warningLabel:  UILabel!
     
+    @objc func keybordWasShow(notification: Notification) {
+        //получаем размер клавиатуры
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue ).cgRectValue.size
+        let contentinsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
     
+        // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
+        self.scrollView?.contentInset = contentinsets
+        scrollView?.scrollIndicatorInsets = contentinsets
+}
+        //Когда клавиатура исчезает
+    @objc func keybordWillBeHidden(notification: Notification) {
+        //Устанавливаем отступ внизу UIScroll View, равный 0
+        let contentInsets = UIEdgeInsets.zero
+        scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+    
+        //Подписываемся на два уведомления: одно приходит при появлении клавиатуры
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keybordWasShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        //Второк - когда она пропадет
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keybordWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-    
     @objc func hideKeybord() {
         self.scrollView?.endEditing(true)
         
     }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         //Жест нажатия
@@ -45,10 +70,6 @@ class LoginFormController: UIViewController {
         }
         return checkResult
     }
-    
-    
-    
-    
     //Функция проверки польователя и пароля
     func checkUsersData() -> Bool {
         guard let login = loginInput.text, let password = passwordInput.text else {return false}
@@ -62,8 +83,6 @@ class LoginFormController: UIViewController {
         return false
     }
     }
-    
-
     @IBAction func loginButtonAction(_ sender: Any) {
         
 //      checkUsersData()
@@ -78,12 +97,9 @@ class LoginFormController: UIViewController {
 //            warningLabel.textColor = .red
 //            warningLabel.text = "password or login incorrect"
        }
- 
-    
     @IBAction func regButtonAction(_ sender: Any) {
     }
     @IBAction func forgotPasButtonAction(_ sender: Any) {
     }
-    
 }
 
