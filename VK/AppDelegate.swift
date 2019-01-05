eadfb107d57e0352705968973eaf7652153a826c
GSMagx <gsmagx@me.com>
2019-01-05 10:44:00 +0100

    update

--- VK/AppDelegate.swift
+++ VK/AppDelegate.swift
@@ -12,6 +12,7 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    
     var window: UIWindow?
   //  var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
     
@@ -25,8 +26,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+  
+        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
